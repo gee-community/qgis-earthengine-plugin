4bdae7de1570db841415c8acdfa84f6616e5a11b
Rodrigo <rprincipe@ciefap.org.ar>
2017-10-30 10:40:50 -0300

    refactor file names (fix typo)

--- qgis-earthengine-plugin/earth_engine_dockwidget.py
+++ qgis-earthengine-plugin/earth_engine_dockwidget.py
@@ -27,7 +27,7 @@ from PyQt4 import QtGui, uic
 from PyQt4.QtCore import pyqtSignal
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
-    os.path.dirname(__file__), 'earh_engine_dockwidget_base.ui'))
+    os.path.dirname(__file__), 'earth_engine_dockwidget_base.ui'))
 
 
 class EarthEnginePluginDockWidget(QtGui.QDockWidget, FORM_CLASS):

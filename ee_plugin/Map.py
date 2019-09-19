c8119f0547a36a7e7f475726d91bd3753c8ee1eb
gena <gennadiy.donchyts@gmail.com>
2019-09-19 14:50:57 -0700

    add __pycache__ to gitignore

--- ee_plugin/Map.py
+++ ee_plugin/Map.py
@@ -1,9 +1,12 @@
 import ee_plugin.utils
 
-def addLayer(image, vis_props=None, name=None, visible=None, opacity=None):
-    if vis_props:
-        image = image.visualize(**vis_props)
+def addLayer(image, vis=None, name=None, visibility=None, opacity=None):
+    if vis:
+        image = image.visualize(**vis)
     if not name:
         name = 'untitled'
+
+    if not opacity:
+        opacity = 1.0
     
-    ee_plugin.utils.add_or_update_ee_image_layer(image, name)
+    ee_plugin.utils.add_or_update_ee_image_layer(image, name, visibility, opacity)

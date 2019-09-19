b2b7a1fe6bb5b517083dc4b87f75ae223aa6c9ad
gena <gennadiy.donchyts@gmail.com>
2019-09-19 14:48:46 -0700

    add examples

---
+++ examples/test-hello-image-3d.py
@@ -0,0 +1,25 @@
+from ee_plugin import Map
+from ee_plugin import utils
+from ee_plugin import palettes
+import ee
+
+dem = ee.Image("JAXA/ALOS/AW3D30_V1_1").select('MED')
+dem = dem.updateMask(dem.gt(0))
+palette = palettes.cb['Pastel1']['7']
+#palette = ['black', 'white']
+rgb = dem.visualize(**{'min': 0, 'max': 5000, 'palette': palette })
+hsv = rgb.unitScale(0, 255).rgbToHsv()
+
+extrusion = 30
+weight = 0.3
+
+hs = ee.Terrain.hillshade(dem.multiply(extrusion), 315, 35).unitScale(10, 250).resample('bicubic')
+
+hs = hs.multiply(1-weight).add(hsv.select('value').multiply(weight))
+hsv = hsv.addBands(hs.rename('value'), ['value'], True)
+rgb = hsv.hsvToRgb()
+
+Map.addLayer(rgb, {}, 'ALOS DEM', True)
+
+
+

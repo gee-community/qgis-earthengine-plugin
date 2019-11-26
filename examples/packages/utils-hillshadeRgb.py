d370a040de44af6a7bcccf42412060ae6ae83d57
gena <gennadiy.donchyts@gmail.com>
2019-11-26 10:52:35 +0100

    added palette in example

--- examples/packages/utils-hillshadeRgb.py
+++ examples/packages/utils-hillshadeRgb.py
@@ -15,9 +15,10 @@ dem = ee.Image("AHN/AHN2_05M_RUW") \
 # palette = palettes.crameri['lisbon']['50']
 # palette = palettes.crameri['oleron']['50']
 # palette = palettes.crameri['roma']['50'][::-1] # reversed
-palette = palettes.crameri['batlow']['100']
+palette = palettes.crameri['batlow']['50']
 
 demRGB = dem.visualize(**{ 'min': -10, 'max': 10, 'palette': palette })
+Map.addLayer(demRGB , {}, 'DEM (RGB)', False)
 
 weight = 0.5 # wegith of Hillshade vs RGB intensity (0 - flat, 1 - HS)
 exaggeration = 5 # vertical exaggeration
@@ -30,10 +31,10 @@ castShadows = False
 
 # no shadows
 rgb = utils.hillshadeRGB(demRGB, dem, weight, exaggeration, azimuth, zenith, contrast, brightness, saturation, castShadows)
-Map.addLayer(rgb, {}, 'DEM (no shadows)', False)
+Map.addLayer(rgb, {}, 'DEM (hillshade)', False)
 
 # with shadows
 castShadows = True
 rgb = utils.hillshadeRGB(demRGB, dem, weight, exaggeration, azimuth, zenith, contrast, brightness, saturation, castShadows)
-Map.addLayer(rgb, {}, 'DEM')
+Map.addLayer(rgb, {}, 'DEM (hillshade, shadows)')
 

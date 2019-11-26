29a13ee71595114acde96d514c202539efddbcb2
gena <gennadiy.donchyts@gmail.com>
2019-11-26 10:49:47 +0100

    added palette in example

--- examples/packages/utils-hillshadeRgb.py
+++ examples/packages/utils-hillshadeRgb.py
@@ -5,19 +5,19 @@ from ee_plugin.packages import utils, palettes
 
 # Note, that this script is quite intense and it may take time to render on EE side
 
-Map.setCenter(4.408241, 52.177595, 18)
+Map.setCenter(4.407, 52.177, 18)
 
 dem = ee.Image("AHN/AHN2_05M_RUW") \
   .resample('bicubic') \
   .convolve(ee.Kernel.gaussian(0.5, 0.25, 'meters'))
 
-#  palette = palettes.crameri['lisbon']['25']
+# See https://github.com/gee-community/ee-palettes for the full list of supported color palettes
+# palette = palettes.crameri['lisbon']['50']
+# palette = palettes.crameri['oleron']['50']
+# palette = palettes.crameri['roma']['50'][::-1] # reversed
+palette = palettes.crameri['batlow']['100']
 
-palette = palettes.crameri['oleron']['50']
-
-#  palette = palettes.crameri['roma']['25'].slice(0).reverse()
-
-demRGB = dem.visualize(**{ 'min': -5, 'max': 15, 'palette': palette })
+demRGB = dem.visualize(**{ 'min': -10, 'max': 10, 'palette': palette })
 
 weight = 0.5 # wegith of Hillshade vs RGB intensity (0 - flat, 1 - HS)
 exaggeration = 5 # vertical exaggeration
@@ -37,4 +37,3 @@ castShadows = True
 rgb = utils.hillshadeRGB(demRGB, dem, weight, exaggeration, azimuth, zenith, contrast, brightness, saturation, castShadows)
 Map.addLayer(rgb, {}, 'DEM')
 
-Map.addLayer(dem, {}, 'DEM (raw)', False)

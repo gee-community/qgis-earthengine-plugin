75cd651d3d9a47dd6ab0ca8d093511a72557c528
gena <gennadiy.donchyts@gmail.com>
2019-12-01 20:49:04 +0100

    the user can't access console while plugin is being installed, provide url to authenticate

--- ee_plugin/ee_auth.py
+++ ee_plugin/ee_auth.py
@@ -2,6 +2,7 @@
 """
 Init and user authentication in Earth Engine
 """
+import urllib.request
 import webbrowser
 from qgis.PyQt.QtWidgets import QInputDialog
 
@@ -21,18 +22,27 @@ def init():
         ee.Initialize()  # retry initialization once the user logs in
 
 
+def tiny_url(url):
+    apiurl = "http://tinyurl.com/api-create.php?url="
+    tinyurl = urllib.request.urlopen(apiurl + url).read()
+    return tinyurl.decode("utf-8")
+
+
 def authenticate():
     auth_url = ee.oauth.get_authorization_url()
     webbrowser.open_new(auth_url)
 
-    print('\nEarth Engine Authentication:\n'
+    print('\nGoogle Earth Engine Authorization:\n'
           'If the web browser does not start automatically, '
-          'please manually browse the URL below:\n"{}"'.format(auth_url))
+          'start it manually and open the following URL:\n"{}"'.format(auth_url))
 
-    token, ok = QInputDialog.getText(None, 'Earth Engine Authentication',
-                                     'To authorize access needed by Earth Engine, follow the\n'
-                                     'instructions and paste the token here:\n\n'
-                                     '(If the web browser does not start automatically\n'
-                                     'see the python shell).')
+    token, ok = QInputDialog.getText(None, 'Authorization',
+                                     'Google Earth Engine Python is not detected on this machine.\n'
+                                     'This plugin uses Google Earth Engine Python API and requires users \n'
+                                     'to be authorized, please follow the instructions in the opened web page\n'
+                                     'and paste the resulting auth token here.\n\n'
+                                     'If the web page does not open automatically, visit the followin link manually:\n\n'
+                                     'URL: ' + tiny_url(auth_url))
+
     if ok and token:
         ee.oauth._obtain_and_write_token(token.strip())

a4457be2848682de5cee9d0f555aa7589fb78379
XavierCLL <xavier.corredor.llano@gmail.com>
2019-11-30 23:14:11 -0800

    Fix paths for extlibs

--- ee_plugin/__init__.py
+++ ee_plugin/__init__.py
@@ -4,13 +4,13 @@ import platform
 import site
 
 if platform.system() == "Windows":
-    extlib_path = '/extlibs_windows'
+    extlib_path = 'extlibs_windows'
 if platform.system() == "Darwin":
-    extlib_path = '/extlibs_darwin'
+    extlib_path = 'extlibs_darwin'
 if platform.system() == "Linux":
-    extlib_path = '/extlibs_linux'
+    extlib_path = 'extlibs_linux'
 
-site.addsitedir(os.path.abspath(os.path.dirname(__file__) + extlib_path))
+site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), extlib_path)))
 
 
 # noinspection PyPep8Naming

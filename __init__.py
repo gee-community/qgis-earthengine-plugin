8a15bd329249e20753cd64d0ba8420c02f24fc47
XavierCLL <xavier.corredor.llano@gmail.com>
2020-07-28 11:49:22 -0700

    Add manually the extra libs path to pkg_resources because pkg_resources doesn't listen to changes on sys.path, fix #62

--- __init__.py
+++ __init__.py
@@ -2,15 +2,22 @@
 import os
 import platform
 import site
+import pkg_resources
 
-if platform.system() == "Windows":
-    extlib_path = 'extlibs_windows'
-if platform.system() == "Darwin":
-    extlib_path = 'extlibs_darwin'
-if platform.system() == "Linux":
-    extlib_path = 'extlibs_linux'
 
-site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), extlib_path)))
+def pre_init_plugin():
+    if platform.system() == "Windows":
+        extlib_path = 'extlibs_windows'
+    if platform.system() == "Darwin":
+        extlib_path = 'extlibs_darwin'
+    if platform.system() == "Linux":
+        extlib_path = 'extlibs_linux'
+    extra_libs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), extlib_path))
+
+    # add to python path
+    site.addsitedir(extra_libs_path)
+    # pkg_resources doesn't listen to changes on sys.path.
+    pkg_resources.working_set.add_entry(extra_libs_path)
 
 
 # noinspection PyPep8Naming
@@ -21,7 +28,9 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
 
-    #
-    from .ee_plugin import GoogleEarthEnginePlugin
+    # load extra python dependencies
+    pre_init_plugin()
 
+    # start
+    from .ee_plugin import GoogleEarthEnginePlugin
     return GoogleEarthEnginePlugin(iface)

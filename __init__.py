9edae270d2a3bf99652d3f8aec95fdf9ec49f507
Gennadii Donchyts <dgena@google.com>
2024-11-03 18:57:15 +0100

    Add support for Google Cloud Projects and authenticate/set project at plugin start

--- __init__.py
+++ __init__.py
@@ -1,10 +1,36 @@
 # -*- coding: utf-8 -*-
 import os
+<<<<<<< HEAD
+=======
+import json
+import pathlib
+import platform
+>>>>>>> 5cda083 (Add support for Google Cloud Projects and authenticate/set project at plugin start)
 import site
 import pkg_resources
 
+PROJECT_NOT_SET_ERROR_MESSAGE = """
+Google Earth Engine project is not set.
+Starting from November 13, 2024, users need to use a Google Cloud project
+to access the Earth Engine platform (https://developers.google.com/earth-engine/guides/transition_to_cloud_projects).
+Please configure Google Cloud project by following instructions:
+https://developers.google.com/earth-engine/guides/command_line#set_a_cloud_project and then restart QGIS.
+"""
 
+<<<<<<< HEAD
 def pre_init_plugin():
+=======
+def add_ee_dependencies():
+    if platform.system() == "Windows":
+        extlib_path = "extlibs_windows"
+    if platform.system() == "Darwin":
+        extlib_path = "extlibs_darwin"
+    if platform.system() == "Linux":
+        extlib_path = "extlibs_linux"
+    extra_libs_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), extlib_path)
+    )
+>>>>>>> 5cda083 (Add support for Google Cloud Projects and authenticate/set project at plugin start)
 
     extra_libs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'extlibs'))
 
@@ -23,12 +49,12 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
 
-    # load extra python dependencies
-    pre_init_plugin()
+    # load EE python dependencies
+    add_ee_dependencies()
 
-    # Initialize the Earth Engine Python API
-    import ee
-    ee.Initialize()
+    # authenticate and initialize EE
+    from . import ee_auth
+    ee_auth.authenticate_and_set_project()
 
     # start
     from .ee_plugin import GoogleEarthEnginePlugin

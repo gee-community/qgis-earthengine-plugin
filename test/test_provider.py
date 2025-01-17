58d71a747faa645bc3318855c94bb7cd7628cf2b
Zac Deziel <zachary.deziel@gmail.com>
2025-01-17 12:23:17 -0800

    Attempt to add test on identify using a raster
    Test fails because of projection issue with ee.Reducer.

--- test/test_provider.py
+++ test/test_provider.py
@@ -19,3 +19,30 @@ def test_point_identify():
     geometry = result[0].geometry()
     assert geometry is not None, "Geometry of identified feature is None"
     assert not geometry.asPoint().isNull(), "Identified point is invalid (0, 0)"
+
+
+def test_raster_identify():
+    image = ee.Image("USGS/SRTMGL1_003")
+    vis_params = {
+        "min": 0,
+        "max": 4000,
+        "palette": ["006633", "E5FFCC", "662A00", "D8D8D8", "F5F5F5"],
+    }
+    Map.addLayer(image, vis_params, "DEM")
+
+    layer = QgsProject.instance().mapLayersByName("DEM")[0]
+    assert layer is not None, "Layer 'DEM' was not added to the project"
+
+    provider = layer.dataProvider()
+    assert provider is not None, "Provider for the layer could not be retrieved"
+
+    point = QgsPointXY(
+        -13553481.96255343593657017, 5907008.49828365817666054
+    )  # Example point in WGS84
+
+    result = provider.identify(
+        point, format=None, boundingBox=None, width=None, height=None
+    )
+    print("Identify result:", result.results())
+    assert result, "Identify operation returned no results"
+    assert len(result.results()) > 0, "Identify operation returned an empty result set"

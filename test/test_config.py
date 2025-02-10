2bdfdac30e8d261b7a7f281a9f90865436b7bed6
Anthony Lukach <anthonylukach@gmail.com>
2025-02-10 12:31:11 -0800

    add tests

--- test/test_config.py
+++ test/test_config.py
@@ -1,12 +1,49 @@
 import json
-from tempfile import TemporaryFile
+from tempfile import NamedTemporaryFile
+from mock import MagicMock
+
+import pytest
 
 from ee_plugin import config
 
 
-def test_config():
-    with TemporaryFile("w") as f:
+@pytest.fixture
+def conf():
+    with NamedTemporaryFile(mode="w") as f:
         json.dump({"project": "qgis-testing"}, f)
-        conf = config.EarthEngineConfig(credentials_path=f.name)
+        f.flush()
+        yield config.EarthEngineConfig(credentials_path=f.name)
 
-        assert conf.project_id == "qgis-testing", "Project ID not set correctly."
+
+def test_config_read_project(conf: config.EarthEngineConfig):
+    """
+    Ensure project ID correctly read from the credentials file.
+    """
+    assert conf.project == "qgis-testing", "Project ID not set correctly."
+
+
+def test_config_write_project(conf: config.EarthEngineConfig):
+    """
+    Ensure project ID correctly written to the credentials file.
+    """
+    conf.project = "new-project"
+    assert conf.project == "new-project", "Project ID not set correctly."
+    with open(conf.credentials_path, "r") as f:
+        assert (
+            json.load(f)["project"] == "new-project"
+        ), "Project ID not written correctly."
+
+
+def test_config_project_signal(conf: config.EarthEngineConfig):
+    """
+    Ensure signal emitted when project written to the credentials file.
+    """
+    m = MagicMock()
+
+    conf.signals.updated.connect(m)
+
+    m.assert_not_called(), "Signal emitted before change."
+
+    conf.project = "new-project"
+
+    m.assert_called_once_with({"project": "new-project"}), "Signal not emitted."

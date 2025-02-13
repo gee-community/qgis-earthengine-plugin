d40bc9e344ba40748e6e14bbc21aada9755be11f
Anthony Lukach <anthonylukach@gmail.com>
2025-02-13 09:23:05 -0800

    Expand tests

--- test/test_ui.py
+++ test/test_ui.py
@@ -1,4 +1,9 @@
-from qgis.PyQt import QtWidgets
+import datetime
+from unittest.mock import create_autospec
+
+import pytest
+from qgis import gui
+from qgis.PyQt import QtWidgets, QtGui
 from ee_plugin.ui import utils, forms
 
 
@@ -24,31 +29,89 @@ def test_get_values():
     dialog.findChild(QtWidgets.QLineEdit, "line_edit").setText("test")
     dialog.findChild(QtWidgets.QCheckBox, "check_box").setChecked(True)
 
-    assert utils.get_values(dialog) == {
+    assert utils.get_dialog_values(dialog) == {
         "line_edit": "test",
         "check_box": True,
     }
 
 
-def test_add_feature_collection_form(qgis_iface):
-    dialog = forms.add_feature_collection_form(iface=qgis_iface)
+@pytest.mark.parametrize(
+    "form_input,expected_form_output",
+    [
+        (
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+            },
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+            },
+        ),
+        (
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+                "start_date": (gui.QgsDateEdit, "2020-01-01"),
+                "end_date": (gui.QgsDateEdit, "2020-12-31"),
+            },
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+                "start_date": "2020-01-01",
+                "end_date": "2020-12-31",
+            },
+        ),
+        (
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+                "start_date": (gui.QgsDateEdit, "2020-01-01"),
+                "end_date": (gui.QgsDateEdit, "2020-12-31"),
+            },
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+                "start_date": "2020-01-01",
+                "end_date": "2020-12-31",
+            },
+        ),
+        (
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+                "viz_color_hex": (gui.QgsColorButton, "#F00"),
+            },
+            {
+                "feature_collection_id": "USGS/WBD/2017/HUC06",
+                "viz_color_hex": "#ff0000",
+            },
+        ),
+    ],
+)
+def test_add_feature_collection_form(qgis_iface, form_input, expected_form_output):
+    callback = create_autospec(forms.add_feature_collection)
+    dialog = forms.add_feature_collection_form(iface=qgis_iface, accepted=callback)
 
-    dialog.findChild(QtWidgets.QLineEdit, "feature_collection_id").setText(
-        "USGS/WBD/2017/HUC06"
-    )
+    # Populate dialog with form_input
+    for key, value in form_input.items():
+        if isinstance(value, tuple):
+            widget_cls, value = value
+        else:
+            widget_cls = QtWidgets.QWidget
 
-    dialog_values = utils.get_values(dialog)
+        widget = dialog.findChild(widget_cls, key)
 
-    assert (
-        dialog_values.items()
-        >= {
-            "feature_collection_id": "USGS/WBD/2017/HUC06",
-            "filter_name": "",
-            "filter_value": "",
-            "start_date": None,
-            "end_date": None,
-            "extent": None,
-            "viz_color_hex": "#000000",
-            "use_util": False,
-        }.items()
-    )
+        if isinstance(widget, gui.QgsDateEdit):
+            widget.setDate(datetime.datetime.strptime(value, "%Y-%m-%d").date())
+        elif isinstance(widget, gui.QgsColorButton):
+            widget.setColor(QtGui.QColor(value))
+        else:
+            widget.setText(value)
+
+    dialog.accept()
+
+    default_outputs = {
+        "filter_name": "",
+        "filter_value": "",
+        "start_date": None,
+        "end_date": None,
+        "extent": None,
+        "viz_color_hex": "#000000",
+        "use_util": False,
+    }
+
+    callback.assert_called_once_with(**{**default_outputs, **expected_form_output})

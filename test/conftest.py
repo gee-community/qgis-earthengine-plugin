d818103da011823b5616f08f7e223399821c1c2e
Anthony Lukach <anthonylukach@gmail.com>
2025-02-14 15:32:28 -0800

    Use QGIS helper for installing plugin

--- test/conftest.py
+++ test/conftest.py
@@ -1,31 +1,11 @@
-import ee
 from pytest import fixture
-from qgis.utils import plugins, isPluginLoaded
+from qgis.utils import isPluginLoaded, startPlugin
 from qgis.gui import QgisInterface
 from PyQt5.QtCore import QSettings, QCoreApplication
 
-from ee_plugin import ee_plugin, config
-
 
 @fixture(scope="session", autouse=True)
-def setup_ee():
-    """Initialize the Earth Engine API."""
-    print("Initializing Earth Engine API...")
-    ee.Initialize()
-    ee.Authenticate(auth_mode="localhost", quiet=True)
-
-
-@fixture(scope="session", autouse=True)
-def ee_config():
-    return config.EarthEngineConfig()
-
-
-@fixture(scope="session", autouse=True)
-def load_ee_plugin(
-    qgis_iface: QgisInterface,
-    setup_ee: None,
-    ee_config: config.EarthEngineConfig,
-):
+def load_ee_plugin(qgis_iface: QgisInterface):
     """Load Earth Engine plugin and configure QSettings."""
 
     # Set QSettings values required by the plugin
@@ -33,10 +13,6 @@ def load_ee_plugin(
     QCoreApplication.setApplicationName("QGIS Testing")
     QSettings().setValue("locale/userLocale", "en")
 
-    # Initialize and register the plugin
-    plugin = ee_plugin.GoogleEarthEnginePlugin(iface=qgis_iface, ee_config=ee_config)
-    plugin.check_version()
-
-    plugins["ee_plugin"] = plugin
+    startPlugin("ee_plugin")
     assert isPluginLoaded("ee_plugin")
     yield qgis_iface
